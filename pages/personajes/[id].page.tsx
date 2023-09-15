f1dc8ff2644f4d5033b4c64f0efa14fff5eb0583
Mili Cerverizzo <milicerve@hotmail.com>
2023-09-15 19:06:08 -0300

    fix/style personajes

--- pages/personajes/[id].page.tsx
+++ pages/personajes/[id].page.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
-import Head from 'next/head'
-import LayoutGeneral from 'dh-marvel/components/layouts/layout-general'
 import { Personaje } from 'interface/personajes'
 import { getCharacter } from 'dh-marvel/services/marvel/marvel.service'
 import { GetServerSideProps, NextPage } from 'next'
-import { Box, Card } from '@mui/material';
+import { Box, Card, Typography } from '@mui/material';
 import Image from 'next/image';
 
 interface Props {
@@ -14,20 +12,18 @@ interface Props {
 const PersonajePage: NextPage<Props> = ({ character }) => {
 
     return (
-        <LayoutGeneral >
-            <Box >
-                <Card sx={{ marginTop: 10, padding: 3, gap: 10, height: "fit-content" }}>
-                    <div>{character.name}</div>
-                    <Image
-                        alt={character.name}
-                        src={character.thumbnail.path.concat(".", character.thumbnail.extension)}
-                        width={300}
-                        height={300}
-                    />
-                    <p>{character?.description}</p>
-                </Card>
-            </Box>
-        </LayoutGeneral>
+        <Box >
+            <Card sx={{ marginTop: 5, padding: 3, gap: 10, height: "fit-content" }}>
+                <Typography variant="h2" fontWeight="bold">{character.name}</Typography>
+                <Image
+                    alt={character.name}
+                    src={character.thumbnail.path.concat(".", character.thumbnail.extension)}
+                    width={300}
+                    height={300}
+                />
+                <p>{character?.description}</p>
+            </Card>
+        </Box>
     )
 }
 
